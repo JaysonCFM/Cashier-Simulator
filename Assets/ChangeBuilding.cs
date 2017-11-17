e24328646e2a161ce029d608c300ec4802dd1d51
Jayson <caublejayson@gmail.com>
2017-11-17 15:40:29 -0500

    Most of the core concept

---
+++ Assets/ChangeBuilding.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeBuilding : MonoBehaviour {
+
+    public float ChangeSoFar;
+
+    public Text text;
+
+    public bool IsDisplay;
+
+    public GenerateMoney howMuchYouStarted;
+
+    public GiveCashier customerChange;
+
+    public GenerateCorrectness calculations;
+
+    private void Update()
+    {
+        if (IsDisplay)
+        {
+            DisplayChange();
+        }
+    }
+
+    public void AddChangeToAmount(float AddChange)
+    {
+        ChangeSoFar += AddChange;
+    }
+
+    private void DisplayChange()
+    {
+        text.text = ChangeSoFar.ToString("C");
+    }
+
+    public void EndTransaction()
+    {
+       if (howMuchYouStarted.Money + ChangeSoFar == customerChange.Money)
+        {
+            ChangeSoFar = 0;
+            calculations.Correct++;
+            howMuchYouStarted.Transaction();
+        }
+       else if (howMuchYouStarted.Money + ChangeSoFar < customerChange.Money)
+        {
+            calculations.Incorrect++;
+            ChangeSoFar = 0;
+            howMuchYouStarted.Transaction();
+        }
+        else if (howMuchYouStarted.Money + ChangeSoFar > customerChange.Money)
+        {
+            calculations.Incorrect++;
+            ChangeSoFar = 0;
+            howMuchYouStarted.Transaction();
+        }
+    }
+}
---
+++ Assets/GenerateCorrectness.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GenerateCorrectness : MonoBehaviour {
+
+    public Text text;
+
+    public int Correct, Incorrect;
+
+    public float Average;
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "Correct: " + Correct.ToString() + " Incorrect: " + Incorrect.ToString() + " % Correct: " + Average.ToString();
+	}
+}
